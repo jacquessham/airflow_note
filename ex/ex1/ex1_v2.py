77fe4458ddab4b8c0e611ece8af40d244b2d1fb2
Jacques Sham <jacquessham2015@gmail.com>
2023-07-09 16:55:59 -0700

    Finish up ex1 readme

--- ex/ex1/ex1_v2.py
+++ ex/ex1/ex1_v2.py
@@ -10,63 +10,6 @@ default_args = {
     'retry_delay': timedelta(minutes=2)
 }
 
-"""
-def load_data(ti):
-    AIRFLOW_HOME = os.getenv('AIRFLOW_HOME')
-    df = pd.read_csv(AIRFLOW_HOME+'/dags/salary.csv')
-    candidates = df['name'].tolist()
-    max_candidate = df[df['salary']==df['salary'].max()].to_dict('records')[0]
-    ti.xcom_push(key='candidates',value=candidates)
-    ti.xcom_push(key='max_candidate',value=max_candidate)
-
-def print_num_candidates(ti):
-    candidates = ti.xcom_pull(task_ids='load_data',key='candidates')
-    print(f"We have {len(candidates)} candidates in the pool...")
-
-def print_highest(ti):
-    max_candidate = ti.xcom_pull(task_ids='load_data',key='max_candidate')
-    print(f"{max_candidate['name']} has the highest salary!"
-          f" Making ${max_candidate['salary']}")
-
-def calculate_trips(ti):
-    max_candidate = ti.xcom_pull(task_ids='load_data',key='max_candidate')
-    dispo_salary = max_candidate['salary']*0.55
-    if dispo_salary < 50000:
-        num_trips = 0
-    else:
-        num_trips = int(dispo_salary/5000)
-    print(f"{max_candidate['name']} may go to Japan up to {num_trips} times!")
-
-with DAG(
-    default_args=default_args,
-    dag_id='ex1_v1',
-    description='The first Airflow pipeline',
-    start_date=datetime(2023,6,1),
-    schedule_interval='0 13 * * Mon-Fri'
-    ) as dag:
-    task1 = BashOperator(
-            task_id='greet',
-            bash_command='echo Hello! Let us look at our candidates:'
-        )
-    task2 = PythonOperator(
-        task_id='load_data',
-        python_callable=load_data
-        )
-    task3 = PythonOperator(
-        task_id='print_highest',
-        python_callable=print_highest
-        )
-    task4 = PythonOperator(
-        task_id='print_num_candidates',
-        python_callable=print_num_candidates
-        )
-    task5 = PythonOperator(
-        task_id='calculate_trips',
-        python_callable=calculate_trips
-        )
-    task1 >> task2 >> [task3, task4]
-    task4 >> task5
-"""  
 @dag(dag_id='ex1_v2',
     default_args=default_args,
     start_date=datetime(2023,6,1),
--- ex/ex1/readme.md
+++ ex/ex1/readme.md
@@ -2,18 +2,29 @@
 In this exercise, we will try to use Airflow to read a CSV file which contains the information of the candidates and try to find the person who has the highest salary. The exercise is a hands-on to try to use the BashOperator and PythonOperator.
 
 ## Plan
-In this exercise, we would do the following:
-<ul>
+In this exercise, we would do the following tasks:
+<ol>
 	<li>Print an introduction on command line</li>
 	<li>Read CSV file</li>
 	<li>Print how many candidates are there</li>
 	<li>Find the person who has the highest salary</li>
 	<li>Calculate how many times he may go to Japan</li>
-</ul>
+</ol>
 
 <br>
 The DAG will looks like this:
 <img src=ex1_dag.png>
+
+<br><br>
+Therefore, the task order should be the following:
+
+```
+task1 >> task2 >> [task3, task4]
+task4 >> task5
+```
+
+<br><br>
+Note: The formula used to calculate how many times he may go to Japan is not a accurate formula, its purpose is simply add some fun into the context. The major goal is to demostrate a task in this DAG.
 
 ## Script
 The filename of the script is <i>ex1_v1.py</i> along with the dataset <i>salary.csv</i>. Both files are expected to be saved under the <i>dags</i> folder in your Airflow folder. 
@@ -29,8 +40,13 @@ AIRFLOW_HOME = os.getenv('AIRFLOW_HOME')
 df = pd.read_csv(AIRFLOW_HOME+'/dags/salary.csv')
 ```
 
+<br><br>
+<b>The scripts are designed to used for Docker hosted Airflow.</b>
+
 ### ex1_v1.py
-Coming Soon...
+The script will execute the task with the mixture of BashOperators and Python Operator, as well as 4 helper functions <i>load_data()</i>, <i>print_num_candidates()</i>, <i>print_highest()</i>, and <i>calculate_trips()</i> to be called for Task 2-5. It will Task instance to pass the data among tasks.
+
 
 ### ex1_v2.py
-Coming Soon...
+The script is the same script as <i>ex1_v1.py</i>, except it is written in the Taskflow API style syntax.
+
